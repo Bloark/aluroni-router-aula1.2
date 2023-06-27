234aae651bf42c63e382a64fcaaec6eff79838d5
Bloark <wildson.luiz@gmail.com>
2023-06-27 00:04:15 -0300

    criado a apgina NotFound e a rota.

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import Inicio from 'pages/Inicio';
 import Sobre from 'pages/Sobre';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from 'components/Footer';
+import NotFound from 'pages/NotFound';
 
 export default function AppRouter() {
   return (
@@ -17,6 +18,7 @@ export default function AppRouter() {
             <Route path='cardapio' element={<Cardapio />} />
             <Route path='sobre' element={<Sobre />} />
           </Route>        
+          <Route path='*' element={<NotFound />} />
         </Routes>
         <Footer/>
       </Router> 
